010743d3982d04b850bfe6d4f813fcf5814b2a12
Benicio Cardozo <beniciocardozomdp@gmail.com>
2022-07-10 21:37:23 -0300

    feat: init db support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,5 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	css: ["@/assets/css/tailwind.css"],
-	buildModules: [
-		"@pinia/nuxt",
-		"@nuxtjs/tailwindcss",
-	],
-	modules: ["@nuxtjs/tailwindcss"],
+	modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
 });
